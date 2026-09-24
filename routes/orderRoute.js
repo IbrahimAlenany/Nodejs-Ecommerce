25574e0733cf2138c0149ca4423784b992dc7c8f
IbrahimAlenany <ib.alenany4@gmail.com>
2026-09-24 23:46:30 +0300

    checkout

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,24 +1,44 @@
 const express = require('express');
-const { 
-    createCashOrder,
-    filterOrderForLoggedUser,
-    findAllOrders,
-    findSpecificOrder
+const {
+  createCashOrder,
+  findAllOrders,
+  findSpecificOrder,
+  filterOrderForLoggedUser,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  checkoutSession,
 } = require('../services/orderServices');
 
-const authServices = require('../services/authService');
+const authService = require('../services/authService');
 
 const router = express.Router();
 
-router.use(authServices.protect);
+router.use(authService.protect);
 
-router.route('/:cartId').post(authServices.allowedTo('user'), createCashOrder);
-router.get('/',
-    authServices.allowedTo('user','admin','manager'),
-    filterOrderForLoggedUser,
-    findAllOrders
+router.get(
+  '/checkout-session/:cartId',
+  authService.allowedTo('user'),
+  checkoutSession
 );
-router.get('/:id', findSpecificOrder)
 
+router.route('/:cartId').post(authService.allowedTo('user'), createCashOrder);
+router.get(
+  '/',
+  authService.allowedTo('user', 'admin', 'manager'),
+  filterOrderForLoggedUser,
+  findAllOrders
+);
+router.get('/:id', findSpecificOrder);
+
+router.put(
+  '/:id/pay',
+  authService.allowedTo('admin', 'manager'),
+  updateOrderToPaid
+);
+router.put(
+  '/:id/deliver',
+  authService.allowedTo('admin', 'manager'),
+  updateOrderToDelivered
+);
 
 module.exports = router;
